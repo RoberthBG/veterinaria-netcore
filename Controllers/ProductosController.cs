b7c9b52a973dec85381819b71016ec06c2d36fec
RoberthBG <roberthbg.2026@hotmail.com>
2026-03-21 14:58:28 -0500

    feat: Se implementó filtros y se modificó la presentación de los productos en forma de tarjetas

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,35 +1,85 @@
 ﻿using Elfie.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using VeterinariaWeb.Models;
 
 namespace VeterinariaWeb.Controllers
 {
     public class ProductosController : Controller
     {
-        private readonly string cadenaConexion = "server=localhost; database=veterinaria;user id=sa;password=sqladmin;TrustServerCertificate=true";
+        private readonly string cadenaConexion = "Server=localhost;database=veterinaria;User Id=sa;password=sqladmin;TrustServerCertificate=true";
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1, string? categoria = null, string? producto = null)
         {
             var listaProductos = obtenerProductos();
-            return View(listaProductos);
+            if (categoria != null)
+                listaProductos = listaProductos.Where(p => p.CategoriaID == Convert.ToInt32(categoria)).ToList();
+            if (producto != null)
+                listaProductos = listaProductos.Where(p => p.Nombre.ToLower().Contains(producto.ToLower()) ||
+                        p.Descripcion.ToLower().Contains(producto.ToLower())).ToList();
+            var listadoCategorias = obtenerCategorias();
+            int registrosPorPagina = 8;
+            int totalProductos = listaProductos.Count;
+            int cantidadPaginas = Convert.ToInt32(Math.Ceiling((double)totalProductos / registrosPorPagina));
+
+            int registrosOmitir = registrosPorPagina * (page - 1);
+
+            ViewBag.categorias = new SelectList(listadoCategorias, "ID", "Nombre", categoria);
+            ViewBag.paginas = cantidadPaginas;
+            ViewBag.paginaActual = page;
+            ViewBag.categoriaActual = categoria;
+            ViewBag.busquedaActual = producto;
+
+            return View(listaProductos.Skip(registrosOmitir).Take(registrosPorPagina));
         }
 
         public IActionResult Detail(int id)
         {
-            var productoBuscado = obtenerProductoPorId(id); 
-            return View();
+            var productoBuscado = obtenerProductoPorId(id);
+            return View(productoBuscado);
         }
 
+        public IActionResult Create()
+        {
+            var categorias = obtenerCategorias();
+            ViewBag.Categorias = new SelectList(categorias, "ID", "Nombre");
+            return View(new Producto());
+        }
 
-        #region . Private Methods .
+        [HttpPost]
+        public IActionResult Create(Producto producto)
+        {
+            var exito = CrearProducto(producto);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var productoBuscado = obtenerProductoPorId(id);
+            var categorias = obtenerCategorias();
+            ViewBag.Categorias = new SelectList(categorias, "ID", "Nombre");
+            return View(productoBuscado);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Producto producto)
+        {
+            var exito = ActualizarProducto(producto);
+            if (exito)
+                return RedirectToAction("Detail", new { id = producto.ID });
+            return View(producto);
+        }
+
+        #region . Private methods .
 
         private List<Producto> obtenerProductos()
         {
             var listaProductos = new List<Producto>();
             using (var conexion = new SqlConnection(cadenaConexion))
             {
-                using (var comando = new SqlCommand("SELECT * FROM Productos", conexion))
+                using (var comando = new SqlCommand("SELECT P.*, CP.Nombre AS NombreCategoria FROM Productos P INNER JOIN CategoriaProductos CP ON P.CategoriaID = CP.ID", conexion))
                 {
                     conexion.Open();
                     using (var lector = comando.ExecuteReader())
@@ -38,48 +88,124 @@ namespace VeterinariaWeb.Controllers
                         {
                             while (lector.Read())
                             {
-                                listaProductos.Add(new Producto
-                                {
-                                    ID = lector.GetInt32(0),
-                                    Nombre = lector.GetString(1),
-                                    Descripcion = lector.GetString(2),
-                                });
+                                listaProductos.Add(convertirReaderEnProducto(lector));
                             }
                         }
                     }
-                }    
+                }
             }
-
-                return listaProductos;
+            return listaProductos;
         }
 
         private Producto obtenerProductoPorId(int id)
-        { 
+        {
             var producto = new Producto();
             using (var conexion = new SqlConnection(cadenaConexion))
             {
-                using (var comando = new SqlCommand("SELECT * FROM Productos WHERE ID = @ID", conexion))
+                using (var comando = new SqlCommand("SELECT P.*, CP.Nombre AS NombreCategoria FROM Productos P INNER JOIN CategoriaProductos CP ON P.CategoriaID = CP.ID WHERE P.ID = @ID", conexion))
                 {
-                   comando.Parameters.AddWithValue("@ID", id);
-                   conexion.Open();
-                   using (var lector = comando.ExecuteReader())
-                   {
-                        if(lector != null && lector.HasRows)
+                    comando.Parameters.AddWithValue("@ID", id);
+                    conexion.Open();
+                    using (var lector = comando.ExecuteReader())
+                    {
+                        if (lector != null && lector.HasRows)
                         {
                             lector.Read();
-                            producto = new Producto()
-                            {
-                                ID = lector.GetInt32(0),
-                                Nombre = lector.GetString(1),
-                                Descripcion = lector.GetString(2)
-
-                            };
+                            producto = convertirReaderEnProducto(lector);
                         }
-                    }     
+                    }
                 }
             }
-            return producto;
 
+            return producto;
+        }
+
+        private List<Categoria> obtenerCategorias()
+        {
+            var listaCategorias = new List<Categoria>();
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("SELECT * FROM CategoriaProductos", conexion))
+                {
+                    conexion.Open();
+                    using (var reader = comando.ExecuteReader())
+                    {
+                        if (reader != null && reader.HasRows)
+                        {
+                            while (reader.Read())
+                                listaCategorias.Add(convertirReaderEnCategoria(reader));
+                        }
+                    }
+                }
+            }
+            return listaCategorias;
+        }
+
+        private bool CrearProducto(Producto producto)
+        {
+            var exito = false;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("INSERT INTO Productos(Nombre, Descripcion, CategoriaID, Precio, Activo) " +
+                    "VALUES(@nombre,@descripcion,@categoria,@precio,'1')", conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", producto.Nombre);
+                    comando.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                    comando.Parameters.AddWithValue("@categoria", producto.CategoriaID);
+                    comando.Parameters.AddWithValue("@precio", producto.Precio);
+                    conexion.Open();
+                    exito = comando.ExecuteNonQuery() > 0;
+                }
+            }
+            return exito;
+        }
+
+        private bool ActualizarProducto(Producto producto)
+        {
+            var exito = false;
+            using (var conexion = new SqlConnection(cadenaConexion))
+            {
+                using (var comando = new SqlCommand("UPDATE Productos SET Nombre = @nombre, Descripcion = @descripcion, " +
+                    "Precio = @precio, CategoriaID = @categoria WHERE ID = @id", conexion))
+                {
+                    comando.Parameters.AddWithValue("@nombre", producto.Nombre);
+                    comando.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+                    comando.Parameters.AddWithValue("@precio", producto.Precio);
+                    comando.Parameters.AddWithValue("@categoria", producto.CategoriaID);
+                    comando.Parameters.AddWithValue("@ID", producto.ID);
+                    conexion.Open();
+                    exito = comando.ExecuteNonQuery() > 0;
+                }
+            }
+            return exito;
+        }
+
+        private Producto convertirReaderEnProducto(SqlDataReader lector)
+        {
+            return new Producto()
+            {
+                ID = lector.GetInt32(0),
+                Nombre = lector.GetString(1),
+                Descripcion = lector.GetString(2),
+                Imagen = lector["PathImagen"] == DBNull.Value ? "" : lector.GetString(3),
+                Precio = lector.GetDecimal(4),
+                CategoriaID = lector.GetInt32(5),
+                Categoria = new Categoria()
+                {
+                    ID = lector.GetInt32(5),
+                    Nombre = lector.GetString(7)
+                }
+            };
+        }
+
+        private Categoria convertirReaderEnCategoria(SqlDataReader lector)
+        {
+            return new Categoria
+            {
+                ID = lector.GetInt32(0),
+                Nombre = lector.GetString(1),
+                Activo = lector.GetString(2),
+            };
         }
 
         #endregion
